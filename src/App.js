9440cb1ef87cd94d816a4d764ff41ae5d73570ad
anubhav patel <anubhavpatel6391@gmail.com>
2023-10-20 19:15:30 +0530

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 
 import './App.css';
-import Data from './components/Data';
+
 import Navbar from './components/Navbar';
 import data from "./data.json"
 import Pagination from './components/Pagination';
 import { useState, useEffect } from 'react';
 function App() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(6);
-
-
-  const lastPostIndex = currentPage * postsPerPage;
-    const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = data.slice(firstPostIndex, lastPostIndex);
-    
+  
    
   return (
     
@@ -22,11 +15,7 @@ function App() {
  
     <Navbar/>
     
-     <Data data={currentPosts}/>
-     <Pagination totalPosts={data.length}
-                postsPerPage={postsPerPage}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}/>
+     
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,72 @@
-import React  from 'react'
-import {CiSearch} from "react-icons/ci"
-import {BsChevronCompactDown} from "react-icons/bs"
+import React from 'react'
+import { CiSearch } from "react-icons/ci"
+import { BsChevronCompactDown } from "react-icons/bs"
 import { Link } from 'react-router-dom'
 import "./Navbar.css"
-import search from "../assests/search.png"
+import data from "../data.json"
 import { useState } from "react"
+import Pagination from './Pagination'
+import Card from './Card'
 const Navbar = () => {
+  const [searchName, setSearchName] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(6);
+
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = data.slice(firstPostIndex, lastPostIndex);
+
+
+
   return (
-    <div className='main-container'>
+    <>
+      <div className='main-container'>
 
-   
-    <div className='header'>
-    <div className='searchbar'>
-    <form style={{display: "flex", justifyContent:"center", alignItems:"center"}}>
-  <input type="text" name="search" placeholder="Search by car name" />
-  <CiSearch size={30}/>
-</form>
-    </div>
-   
 
- <Link className='rel' >Relevance <BsChevronCompactDown/></Link>
- <Link className='brands'>All Brands  <BsChevronCompactDown/></Link>
- 
-</div>
- </div>
+        <div className='header'>
+          <div className='searchbar'>
+            <form style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <input type="text" name="search" placeholder="Search by car name" autoComplete="off" onChange={(e) => setSearchName(e.target.value)}
+              />
+              <CiSearch size={30} />
+            </form>
+          </div>
+
+
+          <Link className='rel' >Relevance <BsChevronCompactDown /></Link>
+          <Link className='brands'>All Brands  <BsChevronCompactDown /></Link>
+
+        </div>
+
+      </div>
+     
+      <div className='data-fetch'>
+        {
+          currentPosts.filter(i => {
+            if (searchName === '') {
+              return i;
+            } else if (i.title.toLowerCase().includes(searchName.toLowerCase())) {
+              return i;
+            }
+          }).map((data) => {
+            return (
+              <Card
+                key={data}
+                title={data.title}
+                image={data.image}
+                year={data.start_production}
+              />
+
+            )
+          })
+        }
+      </div>
+      <Pagination totalPosts={data.length}
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage} />
+    </>
   )
 }
 
